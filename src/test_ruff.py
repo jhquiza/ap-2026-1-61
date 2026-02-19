c4f1d94446a80d92f366319b9de0adc306f702a7
Jhon Quiza <jhquiza@udemedellin.edu.co>
2026-02-19 14:50:41 -0500

    Configuración y prueba de Ruff.

---
+++ src/test_ruff.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def mi_funcion_desordenada(a, b):
+    x = 10  # Variable que no usaremos
+    resultado = a + b
+    return resultado
+
+
+df = pd.DataFrame({"col1": [1, 2], "col2": [3, 4]})
+print("Prueba de Ruff finalizada")
